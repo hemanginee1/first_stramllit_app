1ed7fd2b186742337ce7d7f6106d5d0ee816f62c
hemanginee1 <140069320+hemanginee1@users.noreply.github.com>
2023-07-20 23:46:41 +0530

    Update streamlit_app.py
    unique key

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]   
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-'pip install pandas streamlit'
+unique_key = "my_unique_multiselect"
+options = ["Option 1", "Option 2", "Option 3"]
+selected_options = st.multiselect("Select options:", options, key=unique_key)
