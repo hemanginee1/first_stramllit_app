72ca3974e6bc25f0fa6f55c4a50c57df0d7b7362
hemanginee1 <140069320+hemanginee1@users.noreply.github.com>
2023-07-20 17:36:29 +0530

    Update streamlit_app.py
    CHANGE TYPO IN LINE 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title( 'MY PARANTS NEW HEALTHY DINER')
 Streamlit.header( 'BREAKFAST MENU')
-streamlit.text( 'OMEGA 3 & BLUEBERRY OATMEAL,)
+streamlit.text( 'OMEGA 3 & BLUEBERRY OATMEAL')
 stramlit.text( 'KALE, SPINACH & ROCKET SMOOTHIE')
 streamlit.text( 'HARD-BOILED FREE RANGE EGG')
                
