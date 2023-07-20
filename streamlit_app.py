a10bb9f4b613f36389bd376292a79f4bc7c1fb8d
hemanginee1 <140069320+hemanginee1@users.noreply.github.com>
2023-07-20 17:40:13 +0530

    Update streamlit_app.py
    edit file name streamlit typo mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title( 'MY PARANTS NEW HEALTHY DINER')
-Streamlit.header( 'BREAKFAST MENU')
+streamlit.header( 'BREAKFAST MENU')
 streamlit.text( 'OMEGA 3 & BLUEBERRY OATMEAL')
 stramlit.text( 'KALE, SPINACH & ROCKET SMOOTHIE')
 streamlit.text( 'HARD-BOILED FREE RANGE EGG')
